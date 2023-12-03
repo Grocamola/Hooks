3c456edc2ff79b50d9a0039e283be2f7b0edf754
Janna <faraneh.javadinejad@gmail.com>
2023-12-02 19:48:29 -0500

    fixingTodo

--- src/components/useState/useStateHook.jsx
+++ src/components/useState/useStateHook.jsx
@@ -28,17 +28,23 @@ const UseStateHook = () => {
         e.preventDefault();
         todoItem.desc.length > 3 && setTodo(prev => [...prev, todoItem])
         todoItem.desc.length > 3 && setTodoItem({ status: false, desc: '' }); // Reset the input
-        console.log(todo)
+        // console.log(todo)
+        // uncomment the line above,
         // If you check the console, you'll see useState is 1step behind !
     }
 
     const todoCompleteHandler = (index) => { 
         setTodo((prevTodo) => (
             prevTodo.map((el, i) => (
-                i === index ? { ...el, status: true } : el
+                i === index ? { ...el, status: el.status === true ? false : true } : el
             ))
         ));
     };
+
+    const deleteTodoHandler = (item) => {
+        console.log(item, todo)
+        setTodo(prev => prev.filter(el => el !== item))
+    }
 
     return ( 
         <>
@@ -52,7 +58,23 @@ const UseStateHook = () => {
                     </form>
                     <div className="todos">
                         {todo.map((el, index) => <div className="todoboxEach" key={index}>
-                            <p>{el.desc}</p><button disabled={el.status} onClick={() => todoCompleteHandler(index)}>Done</button>
+                            <p>{el.desc}</p>
+                            <div className="todoboxEach--buttons">
+                            <button 
+                                className="todoboxEach--buttons--gray"
+                                disabled={el.status} 
+                                style={{display: el.status === true ? "none" : 'unset'}} 
+                                onClick={() => todoCompleteHandler(index)}>Ongoing</button>
+                            <button 
+                                className="todoboxEach--buttons--green"
+                                disabled={!el.status} 
+                                style={{display: el.status === false ? "none" : 'unset'}} 
+                                onClick={() => todoCompleteHandler(index)}>Done</button>
+                                <button
+                                    className="todoboxEach--buttons--delete"
+                                    style={{display: el.status === false ? "none" : 'unset'}} 
+                                    onClick={() => deleteTodoHandler(el)}>X</button>
+                            </div>
                         </div>)}
                     </div>
                 </div>
