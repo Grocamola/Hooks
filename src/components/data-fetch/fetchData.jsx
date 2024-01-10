451d827b6f31b19a3aaa16f4304ea14680011d7c
Janna <faraneh.javadinejad@gmail.com>
2024-01-09 19:32:03 -0500

    fetchData-Jan9

--- src/components/data-fetch/fetchData.jsx
+++ src/components/data-fetch/fetchData.jsx
@@ -1,29 +1,66 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import Navbar from '../../containers/navbar';
+
+import cloud from '../../assets/images/cloud.png'
+import sun from '../../assets/images/sun.png'
 
 import './fetchData.css'
 
 
 const FetchData = () => {
 
-    useEffect(() => {
-        // fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&appid=524901&appid=9ab470bf248b295059167622b17c2983`)
-        // .then(response => response.json())
-        // .then(data => console.log(data))
+    var api_key = '524901&appid=9ab470bf248b295059167622b17c2983';
 
-        fetch('https://jsonplaceholder.typicode.com/posts')
+    const [weatherInfo, setWeatherInfo] = useState()
+    const [weatherDownloaded, setWeatherDownloaded] = useState({
+        temp: 0, 
+        icon: '', 
+        city: 'City', 
+        desc: 'Description',
+
+    })
+
+    const date = new Date();
+    const [month, day, year] = [
+    date.getMonth(),
+    date.getDate(),
+    date.getFullYear()
+];
+
+    useEffect(() => {
+        fetch(`//api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&key=${api_key}`)
         .then(response => response.json())
-        .then(json => console.log(json))
+        .then(info => setWeatherInfo(info))
     },[])
+
+    useEffect(() => {
+        if(!weatherInfo) {
+            return
+        } else {
+            let description = weatherInfo.weather[0].description;
+            let temp = `${Math.floor((( weatherInfo.main.temp - 273.15) * 9/5) + 32)}º`
+            let icon = weatherInfo.weather[0].main === "Clouds" ? cloud : sun;
+            let city = weatherInfo.name;
+
+            setWeatherDownloaded({temp: temp, icon: icon, city: city, desc: description})
+        }
+    },[weatherInfo])
+
 
     return ( 
         <>
             <Navbar />
             <div className="fetchData--container">
                 <div className="fetchData--final">
-                    <p>Lets fetch some good stuff!</p>
+                    <div className="fetching-data--weather-container">
+                        <div className="weatherCity">{weatherDownloaded.city}</div>
+                        <div className="weatherIcon"><img src={weatherDownloaded.icon !== "" && weatherDownloaded.icon} alt="icon" /></div>
+                        <div className="weatherTemp"><h2>{weatherDownloaded.temp}</h2></div>
+                        <div className="weatherStatus"><h2>{weatherDownloaded.desc}</h2></div>
+                        <div className="weatherDate">{`${year}/${month}/${day}`}</div>
+                    </div>
                 </div>
-                <div className="fetchData--exercise"><p>Hello</p></div>
+                <div className="fetchData--exercise"><p>Go ahead, this is your box, do some magic!</p></div>
             </div>
         </>
      );
